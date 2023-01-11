a56e8aa54a36e1c00f5cd1e704ffedf5f9b4145c
Alejandro Avilez <aleantonio656@gmail.com>
2023-01-11 00:09:22 -0600

    Lazy loading para la carga de componentes

--- src/app/IMS/ims-routing.module.ts
+++ src/app/IMS/ims-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { UsersComponent } from './pages/users/users.component';
+import { AddProductsComponent } from './pages/add-products/add-products.component';
+import { AddCategoriesComponent } from './pages/add-categories/add-categories.component';
+import { CreateUsersComponent } from './pages/create-users/create-users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'products',
+        component: ProductsComponent,
+      },
+      {
+        path: 'categories',
+        component: CategoriesComponent,
+      },
+      {
+        path: 'users',
+        component: UsersComponent,
+      },
+      {
+        path: 'add-products',
+        component: AddProductsComponent,
+      },
+      {
+        path: 'add-categories',
+        component: AddCategoriesComponent,
+      },
+      {
+        path: 'create-user',
+        component: CreateUsersComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'products'
+      }
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren: () => import('./IMS/ims.module').then( m => m.IMSModule )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
