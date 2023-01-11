bf860cc9b6409b705c0efb3d3aeb864dae48f002
Alejandro Avilez <aleantonio656@gmail.com>
2023-01-10 23:58:48 -0600

    generacion de las paginas de la aplicacion

--- src/app/IMS/ims.module.ts
+++ src/app/IMS/ims.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { IMSRoutingModule } from './ims-routing.module';
+import { AddCategoriesComponent } from './pages/add-categories/add-categories.component';
+import { AddProductsComponent } from './pages/add-products/add-products.component';
+import { CreateUsersComponent } from './pages/create-users/create-users.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { UsersComponent } from './pages/users/users.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddCategoriesComponent,
+    AddProductsComponent,
+    CreateUsersComponent,
+    ProductsComponent,
+    CategoriesComponent,
+    UsersComponent
+  ],
   imports: [
     CommonModule,
     IMSRoutingModule
---
+++ src/app/IMS/pages/create-users/create-users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-users',
+  templateUrl: './create-users.component.html',
+  styles: [
+  ]
+})
+export class CreateUsersComponent {
+
+}
