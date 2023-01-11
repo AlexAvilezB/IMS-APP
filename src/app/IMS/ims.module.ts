fd1aab093df6fb201a5e6fb5045a732f97a0161c
Alejandro Avilez <aleantonio656@gmail.com>
2023-01-10 23:41:58 -0600

    generacion de la aplicacion, instalacion de tailwind y angular material, creacion de carpetas y componente del navbar

---
+++ src/app/IMS/ims.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IMSRoutingModule } from './ims-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IMSRoutingModule
+  ]
+})
+export class IMSModule { }
